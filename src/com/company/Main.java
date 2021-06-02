9ae56c6390aa4b88893cd4c60fa4866b1921cef7
swalcoff <scwalcof@ucsc.edu>
2021-06-02 15:50:43 -0700

    first commit with java and jenkins files

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is going to get printed");
+        System.out.println("This will get printed too");
+    }
+}

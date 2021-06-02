74279d5ddd1fd61e351946f23a43eae88f4e5d78
swalcoff <scwalcof@ucsc.edu>
2021-06-02 16:52:14 -0700

    minor modification made

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is going to get printed");
-        System.out.println("This will get printed too");
+        System.out.println("minor modification");
     }
 }
